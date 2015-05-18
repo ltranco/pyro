c3aae0e4dc9d788db657fb7f6318be3128a74341
Long Tran <v.long128@gmail.com>
2015-05-18 12:32:02 -0700

    OMFG it works now

--- v2/js/pyro.js
+++ v2/js/pyro.js
@@ -1,15 +1,19 @@
 var hasFlash = ((typeof navigator.plugins != "undefined" && typeof navigator.plugins["Shockwave Flash"] == "object") || (window.ActiveXObject && (new ActiveXObject("ShockwaveFlash.ShockwaveFlash")) != false));
 var firebase_url = "";
+var firebase_langOpt = "";
 var userId = Math.floor(Math.random() * 9999999999).toString();
 var pythonDefault = "print 'Welcome to PyroPad in Python!'";
 var javaDefault = "//Please keep your public class name as 'solution'\npublic class solution {\n  public static void main(String[] args) {\n    System.out.println(\"Welcome to PyroPad in Java!\");\n  }\n}";
 var socket = io('https://hidden-inlet-2774.herokuapp.com/');
 var codeMirror = CodeMirror(document.getElementById('firepad-container'), {lineNumbers: true, theme: 'monokai', mode: 'python', userId:userId});
 var codeMirrorOutput = CodeMirror(document.getElementById('firepad-container-output'), {lineNumbers: true, theme: 'monokai', mode: 'text/plain', readOnly: "nocursor"});
+var codeMirrorLangOpt = CodeMirror(document.getElementById('dummyLangOpt'), {});
 var firepadRef = getRef();
+var firepadLangRef = new Firebase(firebase_langOpt);
 var outputRef = new Firebase(firebase_url);
 var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {defaultText: pythonDefault});
 var outputpad = Firepad.fromCodeMirror(outputRef, codeMirrorOutput, {defaultText: "OUTPUT"});
+var langOptPad = Firepad.fromCodeMirror(firepadLangRef, codeMirrorLangOpt, {defaultText: "OUTPUT"});
 var compiler = 'python';
 var filename = 'solution.py';
 var setMode = 'python';
@@ -19,7 +23,7 @@ var ext = {'python':'py', 'haskell':'hs', 'java':'java', 'c':'c', 'c++':'cpp'};
 var templateCode = {'python':pythonDefault, 'java':javaDefault};
 
 function fire() {
-  var currLang = localStorage.getItem("currLang");
+  firepadLangRef.update({"currLang":"python"});
   /*if(currLang) {
     $("#languageselected").html((currLang.charAt(0).toUpperCase() + currLang.slice(1)) + "<strong class=\"caret\"></strong>");
     codeMirror.setOption("mode", mode[currLang]);
@@ -34,10 +38,10 @@ $("#languageoption li a").click(function(){
   $("#languageselected").html($(this).text() + "<strong class=\"caret\"></strong>");
   var lang = $(this).text().toLowerCase();
   //localStorage.setItem("currLang", lang);
-  /*if(lang == "python") 
-    firepadRef.update({"currLang":"python"});
+  if(lang == "python") 
+    firepadLangRef.update({"currLang":"python"});
   else if(lang == "java")
-    firepadRef.update({"currLang":"java"});*/
+    firepadLangRef.update({"currLang":"java"});
   setMode = mode[lang];
   compiler = compile[lang];
   filename = 'solution.' + ext[lang];
@@ -47,9 +51,16 @@ $("#languageoption li a").click(function(){
   console.log(lang + " " + compiler + " " + filename);
 });
 
-firepad.on("synced", function(isSynced) {
-  $("#languageselected").html(codeMirror.getOption("mode") + "<strong class=\"caret\"></strong>");
-  });
+firepadLangRef.child("currLang").on("value", function(snapshot) {
+  var lang = snapshot.val();
+  $("#languageselected").html((lang.charAt(0).toUpperCase() + lang.slice(1)) + "<strong class=\"caret\"></strong>");
+  setMode = mode[lang];
+  compiler = compile[lang];
+  filename = 'solution.' + ext[lang];
+
+  codeMirror.setOption("mode", setMode);
+  console.log(lang + " " + compiler + " " + filename);
+});
 
 socket.on('output', function(output) {
   $("#runButton").html("Run &#9658;");
@@ -81,8 +92,10 @@ function getRef() {
   }
   if(typeof console !== 'undefined') {
     firebase_url = ref.toString() + "--output";
+    firebase_langOpt = ref.toString() + "--langOpt";
   }
   console.log('Firebase data: ', ref.toString());
+  console.log("opt: " + firebase_langOpt);
   return ref;
 }
 
